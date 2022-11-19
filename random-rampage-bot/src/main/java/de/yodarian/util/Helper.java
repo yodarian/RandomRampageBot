088ca0626a4a2d9ba3a4a51391cb70f1e10f1d41
flokas <florian.kastner9678@googlemail.com>
2022-11-19 12:43:20 +0100

    - removed bot token - renamed root folder

--- random-rampage-bot/src/main/java/de/yodarian/util/Helper.java
+++ random-rampage-bot/src/main/java/de/yodarian/util/Helper.java
@@ -1,5 +1,10 @@
 package de.yodarian.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +27,14 @@ public class Helper
         Map<String, String> copy = new HashMap<String, String>();
         copy.putAll(map);
         return copy;
-    }  
+    }
+
+    public static String getBotToken() throws IOException
+    {
+        String workingDir = System.getProperty("user.dir");
+        Path filePath = Paths.get(workingDir + File.separator + "random-rampage-bot/src/main/java/de/yodarian/config/dc.token");
+
+        return Files.readString(filePath);
+    }
     
 }
